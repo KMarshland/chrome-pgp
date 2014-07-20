6942894711eaa289b7737818d09454739d863d04
Kai <kaimarshland@gmail.com>
2014-07-20 14:46:07 -0700

    Added import/expport of keys

--- options.js
+++ options.js
@@ -72,6 +72,60 @@ function generate_keys() {
   localStorage['u'] = JSON.stringify(key.u);
 }
 
+function exportKey() {
+  /*var key = {
+    pkey: localStorage['pkey'],
+    p: /*JSON.parse*(localStorage['p']),
+    q: /*JSON.parse*(localStorage['q']),
+    d: /*JSON.parse*(localStorage['d']),
+    u: /*JSON.parse*(localStorage['u'])
+  };
+  
+  return '{' +
+    'pkey: "' + key.pkey + '",' +
+    'p: ' + key.p + ',' +
+    'q: ' + key.q + ',' +
+    'd: ' + key.d + ',' +
+    'u: ' + key.u + '' +
+  '}';*/
+  
+  var key = {
+    pkey: localStorage['pkey'],
+    p: JSON.parse(localStorage['p']),
+    q: JSON.parse(localStorage['q']),
+    d: JSON.parse(localStorage['d']),
+    u: JSON.parse(localStorage['u'])
+  };
+  
+  return JSON.stringify(key);
+}
+
+function middleOfStr(str){
+  return str.substr(1, str.length - 2);
+}
+
+function importKey(str) {
+  var isAllowed = true;
+  
+  /*var key = eval(str);
+  
+  if (str.substr(0, 1) == '{' && str.substr(-1, 1) == '}') {
+    str = middleOfStr(str);
+    
+  }*/
+  var key = JSON.parse(str);
+  
+  if (isAllowed) {
+    
+    localStorage['pkey'] = key.pkey;
+    localStorage['p'] = JSON.stringify(key.p);
+    localStorage['q'] = JSON.stringify(key.q);
+    localStorage['d'] = JSON.stringify(key.d);
+    localStorage['u'] = JSON.stringify(key.u);
+  }
+  
+}
+
 function showGenerateDiv() {
   document.getElementById('generateDiv').style.display = 'block';
 }
@@ -83,6 +137,38 @@ function hideGenerateDiv() {
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
 
-document.querySelector('#generate').addEventListener('click', showGenerateDiv);
+document.querySelector('#generate').addEventListener('click', function(){
+  if (confirm('This will change the keys used to encrypt your messages. ' +
+      'You will be unable to see your past messages and unable to decrypt new messages until they have your new public key.' +
+      '\n\nIf you aren\'t sure, then don\'t do it.' +
+      '\n\nThere is no going back.'      
+  )){
+    showGenerateDiv();
+  }
+});
+
+document.querySelector('#exportKey').addEventListener('click', function(){
+  if (confirm('Never send your private key to anybody. This will allow them to decrypt messages meant for you. ' +
+      '\n\nIf you just want to send someone encrypted messages, all you need to give them is your public key.'    
+  )){
+    //alert("Your key: " + exportKey());
+    document.getElementById('private-key').innerHTML = exportKey();
+  }
+});
+
+document.querySelector('#importKey').addEventListener('click', function(){
+  if (confirm('Don\'t do this unless you actually know what you\'re doing. Seriously. ' +
+      ''    
+  )){
+    //alert("Your key: " + exportKey());
+    document.getElementById('private-key').innerHTML = '<textarea id="keyToImport"></textarea><button id="actuallyImport">Save</button>';
+    document.querySelector('#actuallyImport').addEventListener('click', function(){
+      importKey(document.getElementById('keyToImport').value);
+      document.getElementById('private-key').innerHTML = '';
+    });
+    restore_options();
+  }
+});
+
 document.querySelector('#generateYes').addEventListener('click', function(){generate_keys();hideGenerateDiv();});
 document.querySelector('#generateNo').addEventListener('click', hideGenerateDiv);
