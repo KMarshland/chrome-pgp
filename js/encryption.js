a64f3c47e3f290161ade4d63965aa77b84c12e68
Kai Marshland <kaimarshland@gmail.com>
2015-07-23 01:28:00 -0700

    Created a full encrypter

--- js/encryption.js
+++ js/encryption.js
@@ -105,6 +105,52 @@ $( document ).ready(function(){
             return CryptoJS.AES.encrypt('', '').formatter.parse(str);
         }
 
+        function secureRandom(num){
+            num = num || 128;
+            var r = window.crypto.getRandomValues(new Uint8Array(num + 8));
+            return hex2s(Array.prototype.join.apply(r, [''])).substr(0,num);
+        }
+
+        function encrypt(opts, callback){
+            if (!opts.message){
+                return callback("No message provided");
+            }
+
+            if (!opts.key){
+                return callback("No key");
+            }
+
+            //generate a key and symmetrically encrypt the corpus with that
+            var symKey = secureRandom(256);
+            symmetricEncrypt({
+                key: symKey,
+                message: opts.message
+            }, function(err, encrypted){
+                if (err){
+                    return callback(err);
+                }
+
+                //asymmetrically encrypt the generated key
+                encryptWithKey({
+                    message: symKey,
+                    key: opts.key
+                }, function(err, messageKey){
+                    if (err){
+                        return callback(err);
+                    }
+
+                    callback(null, {
+                        key: messageKey,
+                        message: encrypted
+                    });
+                })
+            })
+        }
+
+        function decrypt(opts, callback){
+
+        }
+
         return {
             generatePGPKeypair: generatePGPKeypair,
             encryptWithKey: encryptWithKey,
@@ -113,7 +159,10 @@ $( document ).ready(function(){
             symmetricDecrypt: symmetricDecrypt,
             aesParse: aesParse,
             aesStringify: aesStringify,
-            hex2s: hex2s
+            hex2s: hex2s,
+            secureRandom: secureRandom,
+            encrypt: encrypt,
+            decrypt: decrypt
         };
     })();
 
@@ -144,5 +193,37 @@ $( document ).ready(function(){
                 ENCRYPTED = res;
             });
         });
+
+        window.benchmark = function(opts, callback){
+            var start = new Date().valueOf();
+            var whenEnded = function(){
+                var totalTime = new Date().valueOf() - start;
+                callback({
+                    totalTime: (totalTime/1000).toFixed(1) + 's',
+                    averageTime: (totalTime/opts.trials).toFixed(3) + 'ms'
+                })
+            };
+
+            if (opts.async){
+                var trialNum = 0;
+                var next = function() {
+                    trialNum ++;
+                    if (trialNum > opts.trials){
+                        return whenEnded();
+                    }
+                    opts.f(function () {
+                        next();
+                    });
+                };
+                next();
+            } else {
+                for (var i = 0; i < opts.trials; i++){
+                    opts.f();
+                }
+                whenEnded();
+            }
+
+
+        }
     })(true);
 });
